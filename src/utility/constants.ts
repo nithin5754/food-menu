8fd702eb600f61001b89679c6a908ebe4ee8feb2
Nithin Joji0756 <nithinjoji0756@gmail.com>
2025-03-13 01:42:52 +0530

    rectify error-5

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,3 +1,3 @@
 
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : `${location.origin}`;
+  location.hostname === "localhost" ? "http://localhost:3000" : 'https://food-menu-backend-kehz.onrender.com';
