0cd138cfbcab74e757d91255c407b1ea80453439
Nithin Joji0756 <nithinjoji0756@gmail.com>
2025-03-13 01:17:23 +0530

    rectify error

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,4 +1,3 @@
 
-
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+  location.hostname === "localhost" ? "http://localhost:3000" : `${location.origin}/api`;
